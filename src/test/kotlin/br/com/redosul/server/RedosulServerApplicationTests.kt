2616e4308aa6d132cbec5b18bbe6493cf4912a96
Guilherme Salustiano <guissalustiano@gmail.com>
2022-12-07 11:52:57 -0300

    Initial commit

---
+++ src/test/kotlin/br/com/redosul/server/RedosulServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.redosul.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RedosulServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
